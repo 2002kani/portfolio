0d1552402c147b5d9079e06e51a362f856972953
2002kani <kanikaradag02@gmail.com>
2024-08-11 11:07:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Kani Karadag"],
+    typeSpeed: 220,
+    backSpeed: 150,
+    loop: true
+});
+    
+
+const btns = document.querySelectorAll(".tab-btn");
+const überMich = document.querySelector(".übermich");
+const contents = document.querySelectorAll(".content");
+
+überMich.addEventListener("click", function(e){
+    const id = e.target.dataset.id;
+
+    if(id){
+        btns.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+
+        contents.forEach(function(content){
+            content.classList.remove("active");
+        });
+
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+});
